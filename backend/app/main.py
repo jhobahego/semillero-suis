613d773037292a86e0df0b76e5cb4cb087473057
Jhon Hernandez <jhobahego16@hotmail.com>
2023-10-29 18:43:48 -0500

    Add: Rutas de usuario y autenticación

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from routes import user, auth
+
 app = FastAPI()
 
 app.title = "Api del semillero SUIS"
@@ -11,8 +13,10 @@ app.version = "0.0.1"
 app.openapi_tags = [
     {"name": "Home", "description": "Documentation"},
     {"name": "Users", "description": "Users routes"},
-    {"name": "Projects", "description": "Projects routes"},
 ]
+
+app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get(
