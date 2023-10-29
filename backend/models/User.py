d8f84d071f139a22e31ba12621779d94459fff08
Jhon Hernandez <jhobahego16@hotmail.com>
2023-10-29 18:28:21 -0500

    Add: Modelo de usuario y roles

---
+++ backend/models/User.py
@@ -0,0 +1,43 @@
+from sqlalchemy import (
+    Boolean,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Enum as SqlAlchemyEnum,
+)
+
+from enum import Enum
+from db import Base
+
+
+class RolName(str, Enum):
+    ADMIN = "ADMIN"
+    USER = "USER"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100))
+    lastname = Column(String(100))
+    email = Column(String(150), unique=True, index=True)
+    hashed_password = Column(String(250))
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+
+
+class Roles(Base):
+    __tablename__ = "roles"
+
+    rol_id = Column(Integer, primary_key=True, index=True)
+    name = Column(SqlAlchemyEnum(RolName, create_constraint=True), default=RolName.USER)
+
+
+class UserRol(Base):
+    __tablename__ = "users_roles"
+
+    user_rol_id = Column(Integer, primary_key=True, index=True)
+    rol_id = Column(Integer, ForeignKey("roles.rol_id"))
+    user_id = Column(Integer, ForeignKey("users.user_id"))
