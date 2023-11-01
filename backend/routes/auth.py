e62f4ca5e5b788347f107155c786600e47fde24b
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-01 17:55:39 -0500

    Fix: Acceso al id de usuario en login

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -32,7 +32,7 @@ def login_access_token(
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": create_access_token(
-            user.user_id, expires_delta=access_token_expires
+            user.id, expires_delta=access_token_expires
         ),
         "token_type": "bearer",
     }
