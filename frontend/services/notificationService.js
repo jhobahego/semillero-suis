4bc27810e23f913b0641786e55d29fc5c006f83c
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-08 13:30:00 -0500

    Add: Servicio para sweetalert2

---
+++ frontend/services/notificationService.js
@@ -0,0 +1,23 @@
+export const notificationUtilities = {
+  toast(title, text, icon, timer = 0) {
+    const swalertOptions = {
+      title,
+      text,
+      icon
+    }
+
+    if (timer > 0) {
+      swalertOptions.timer = timer
+    }
+
+    return Swal.fire(swalertOptions)
+  },
+
+  success(text) {
+    this.toast("Operacion existosa", text, "success")
+  },
+
+  error(text) {
+    this.toast("Se ha producido un error", text, "error")
+  }
+}
