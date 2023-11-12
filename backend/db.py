6cbc22f371752e696d840a00f24a5df5eabb557e
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-12 18:09:43 -0500

    Add: Configuracion para tests

--- backend/db.py
+++ backend/db.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import sessionmaker
 
 from decouple import config
 
-SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
+mode = config("RUN_ENV")
+
+SQLALCHEMY_DATABASE_URL = ""
+
+if mode == "test":
+    SQLALCHEMY_DATABASE_URL = config("TEST_DATABASE_URL")
+else:
+    SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
