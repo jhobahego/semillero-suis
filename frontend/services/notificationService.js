8448c719e2dc8febd45bc2a35bfda5c2f3eedc1e
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-21 17:20:25 -0500

    Add: Notificación popup

--- frontend/services/notificationService.js
+++ frontend/services/notificationService.js
@@ -15,6 +15,40 @@ export const notificationUtilities = {
     return Swal.fire(swalertOptions)
   },
 
+  popup(title, text, timer = 3000, color = 'white', position = 'top-end', icon) {
+    const options = {
+      title,
+      text,
+      icon,
+      position,
+      showClass: {
+        popup: `
+        animate__animated
+        animate__fadeInUp
+        animate__faster
+      `,
+      },
+      hideClass: {
+        popup: `
+        animate__animated
+        animate__fadeOutDown
+        animate__faster
+      `,
+      },
+      grow: 'row',
+      width: 300,
+      showConfirmButton: false,
+      background: '#00447b',
+      color
+    }
+
+    if (timer > 0) {
+      options.timer = timer
+    }
+
+    return Swal.fire(options)
+  },
+
   success(text) {
     this.toast("Operacion existosa", text, "success")
   },
