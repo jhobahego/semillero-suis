315d4c74d979a98122f9e29fd4da5ebd2b0f7264
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-24 17:26:43 -0500

    Refactor: Validación endpoint de creación de evento

--- backend/routes/event.py
+++ backend/routes/event.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from crud.crud_event import event
+from crud.crud_user import user as crud_user
 from schemas.Event import EventCreate, EventResponse
 from config.deps import get_current_active_admin, get_db
 
@@ -16,11 +17,13 @@ router = APIRouter()
     dependencies=[Depends(get_current_active_admin)],
 )
 def create_event(event_data: EventCreate, db: Session = Depends(get_db)):
-    try:
-        new_event = event.create(db=db, obj_in=event_data)
-
-        return new_event
-    except Exception as e:
+    manager_user = crud_user.get(db=db, id=event_data.manager_id)
+    if manager_user is None:
         raise HTTPException(
-            status_code=400, detail=f"Error al crear el evento: {str(e)}"
+            status_code=404,
+            detail="El usuario no encontrado, debes usar un usuario registrado",
         )
+
+    new_event = event.create(db=db, obj_in=event_data)
+
+    return new_event
