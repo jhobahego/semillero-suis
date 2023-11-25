2feb2204266c16226827a533bcce67207c836efb
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-24 20:10:41 -0500

    Refactor: Al crear evento ahora se guarda en BD

--- frontend/utils/calendar.js
+++ frontend/utils/calendar.js
@@ -2,6 +2,8 @@ import { Modal } from 'bootstrap';
 import Swal from 'sweetalert2';
 
 import { calendar } from '../views/admin/admin';
+import { createEvent } from '../services/eventService';
+import axiosInstance from '../services/axios';
 
 const modal = new Modal(document.getElementById("myModal"));
 const formCalendar = document.getElementById("formCalendar");
@@ -10,8 +12,23 @@ document.getElementById("cancelBtn").addEventListener("click", () => {
   modal.hide();
 });
 
-export function handleDateClick(info) {
+export async function handleDateClick(info) {
   setDefaultDates(info.dateStr);
+
+  // Se cargan los usuarios de la base de datos en select del responsable y sus datos en los campos correspondientes
+  // para que solo facilitar el llenado del formulario
+  const usuarios = await obtenerUsuarios();
+  const managerSelect = document.getElementById("manager");
+
+  let optionsHTML = '';
+
+  usuarios.forEach(usuario => {
+    const { id, name } = usuario;
+    const option = `<option value="${id}">${name}</option>`;
+    optionsHTML += option;
+  });
+
+  managerSelect.innerHTML = optionsHTML;
 
   const addButton = document.querySelector("#formCalendar button[type='submit']");
   const deleteButton = document.querySelector("#formCalendar button.btn-danger");
@@ -24,18 +41,51 @@ export function handleDateClick(info) {
   formCalendar.addEventListener("submit", handleFormSubmit);
 }
 
-function handleFormSubmit(event) {
+async function handleFormSubmit(event) {
   event.preventDefault();
 
+  const eventType = document.getElementById("selectType").value;
+  const managerId = document.getElementById("manager").value;
+  const title = document.getElementById("title").value;
   const startDate = document.getElementById("start-date").value;
   const endDate = document.getElementById("end-date").value;
   const color = document.getElementById("color").value;
   const descripcion = document.getElementById("descripcionEvento").value;
+  const event_location = document.getElementById("event-location").value;
+  const duration = document.getElementById("duration").value;
 
-  if (startDate === '' || endDate === '' || color === '' || descripcion === '') {
+  if (
+    managerId === '' ||
+    title === '' ||
+    startDate === '' ||
+    endDate === '' ||
+    color === '' ||
+    descripcion === '' ||
+    event_location === '' ||
+    duration === ''
+  ) {
     showWarningAlert();
   } else {
-    addCalendarEvent(descripcion, startDate, endDate, color);
+
+    try {
+      const eventData = {
+        activity_type: eventType,
+        title: title,
+        description: descripcion,
+        manager_id: parseInt(managerId),
+        color: color,
+        start: startDate,
+        finished_at: endDate !== '' ? endDate : null,
+        event_location: event_location,
+        duration: duration
+      };
+      const { data } = await createEvent(eventData)
+      // console.log(data);
+    } catch (error) {
+
+    }
+
+    addCalendarEvent(title, startDate, endDate, color);
 
     modal.hide();
     showSuccessAlert();
@@ -49,10 +99,13 @@ function addCalendarEvent(title, startDate, endDate, color) {
 }
 
 function resetFormInputs() {
+  document.getElementById("title").value = '';
   document.getElementById("start-date").value = '';
   document.getElementById("end-date").value = '';
   document.getElementById("color").value = '';
   document.getElementById("descripcionEvento").value = '';
+  document.getElementById("event-location").value = '';
+  document.getElementById("duration").value = '';
 }
 
 function setDefaultDates(dateStr) {
@@ -82,4 +135,13 @@ function showSuccessAlert() {
     text: 'El evento se ha agendado con éxito',
     icon: 'success',
   });
+}
+
+async function obtenerUsuarios() {
+  try {
+    const { data } = await axiosInstance.get("/users")
+    return data
+  } catch (error) {
+
+  }
 }
