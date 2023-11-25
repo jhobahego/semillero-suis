6d0fa9d3c217055464e529aa2f33f2553fb5163a
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-25 17:58:17 -0500

    Add: Servicio obtener usuario

--- frontend/services/userServices.js
+++ frontend/services/userServices.js
@@ -3,3 +3,7 @@ import axiosInstance from "./axios"
 export const getUsers = async () => {
   return await axiosInstance.get("/users")
 }
+
+export const getUser = async (userId) => {
+  return await axiosInstance.get(`/users/${userId}`)
+}
