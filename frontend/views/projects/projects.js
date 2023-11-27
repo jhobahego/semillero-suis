243128f9e6e9a11e897a8b5fcc643ec4439e4e47
Jhon Hernandez <jhobahego16@hotmail.com>
2023-11-27 16:51:00 -0500

    Fix: Mostrar y ocultar el modal sin cdn

--- frontend/views/projects/projects.js
+++ frontend/views/projects/projects.js
@@ -1,5 +1,20 @@
-import * as bootstrap from 'bootstrap'
+import { Modal } from 'bootstrap'
 
 import { manageSession } from "../../utils/navbar.js";
 
 manageSession()
+
+const modal = new Modal(document.getElementById("myModal"))
+
+const closeCreateFormBtn = document.getElementById("closeCreateForm")
+const showCreateFormBtn = document.getElementById("createFormBtn")
+
+// Muestra el modal al dar click en boton de crear proyecto
+showCreateFormBtn.addEventListener("click", () => {
+  modal.show()
+})
+
+// Oculta el modal al dar click en el boton de cierre
+closeCreateFormBtn.addEventListener("click", () => {
+  modal.hide()
+})
